9c46c4249a6e17aca544bba3e1ce3f588ccd786d
Rod Monk <roderickmonk@gmail.com>
2017-07-07 13:50:01 -0700

    First commit

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
 "use strict";
 exports.__esModule = true;
-var hft = require("../build/Release/HftEngine");
-// Main package function
-//function computeOrders(input) {
-// Returns true if the input is either undefined, null, or empty, false otherwise
-//    return (input === undefined || input === null || input === '');
-//}
-// Make the main function available to other packages that require us
-// module.exports = computeOrders;
-exports.engine = hft.Engine;
+exports.hft = require("./build/Release/HftEngine");
--- index.ts
+++ index.ts
@@ -1,12 +1,1 @@
-const hft = require("../build/Release/HftEngine");
-
-// Main package function
-//function computeOrders(input) {
-    // Returns true if the input is either undefined, null, or empty, false otherwise
-//    return (input === undefined || input === null || input === '');
-//}
-
-// Make the main function available to other packages that require us
-// module.exports = computeOrders;
-
-export const engine = hft.Engine;
+export const hft = require("./build/Release/HftEngine");
