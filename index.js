8db1682e944f2b8ec3d16e4b3d3b2d0d99de0213
Rod Monk <roderickmonk@gmail.com>
2017-07-07 12:22:43 -0700

    First commit

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-hft = require("../build/Release/HftEngine");
-
+"use strict";
+exports.__esModule = true;
+var hft = require("../build/Release/HftEngine");
 // Main package function
 //function computeOrders(input) {
-    // Returns true if the input is either undefined, null, or empty, false otherwise
+// Returns true if the input is either undefined, null, or empty, false otherwise
 //    return (input === undefined || input === null || input === '');
 //}
-
 // Make the main function available to other packages that require us
-module.exports = computeOrders;
-
-		this.engine = new this.hft.Engine(
-			botConfig.feeRate,
-			botConfig.quantityLimit,
-			botConfig.updateThreshold,
-			botConfig.placeThreshold,
-			tuning1,
-			tuning2);
+// module.exports = computeOrders;
+exports.engine = hft.Engine;
